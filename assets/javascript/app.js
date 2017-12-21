edb946b1c691a7f6d265c9c8c4804811115a5bcf
Mateo Vargas <mateovargas@Mateos-MacBook-Pro.local>
2017-12-20 21:16:01 -0800

    working on getting the table to display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,29 @@
 var coins_ticker = "";
-
 var ticker_tracker = 0;
+var coins_price = {};
+var current_name = "";
+var current_role = "";
+var start_date = "";
+var current_rate = "";
+var total_gain = '';
+var total_loss = '';
 
+
+//Code to initialize Firebase. This is Mateo's personal firebase config.
+var config = {
+	apiKey: "AIzaSyDf35aVp6HQZ9iZTXzPGBO5kI0-T5oMyGc",
+    authDomain: "cryptocharters-48619.firebaseapp.com",
+    databaseURL: "https://cryptocharters-48619.firebaseio.com",
+    projectId: "cryptocharters-48619",
+    storageBucket: "cryptocharters-48619.appspot.com",
+    messagingSenderId: "949624960748"
+};
+
+//Initializes Firebase using the configuration from Mateo
+firebase.initializeApp(config);
+
+//Initialized variables for use through
+var database = firebase.database();
 
 $.ajax({
 	url: 'https://api.coinmarketcap.com/v1/ticker/',
@@ -12,6 +34,14 @@ $.ajax({
 
 	console.log(response);
 	displayTicker();
+
+	for(var j = 0; j < coins_ticker.length; j++){
+
+		var key = coins_ticker[j].symbol;
+		coins_price[key] = coins_ticker[j].price_usd;
+
+	}
+	console.log(coins_price);
 
 });
 
@@ -38,6 +68,89 @@ function displayTicker(){
 	}
 
 };
+
+//Event listener that runs function upon clicking submit.
+$('#add-trade-button').on('click', function(){
+
+  //Prevents the page from reloading.
+  event.preventDefault();
+
+  //Gets the inputted values for the employees from the form and increments
+  //employee count.
+  current_date = $('#date-input').val();
+  current_symbol = $('#symbol-input').val();
+  current_price = $('#price-input').val();
+  current_units = $('#units-input').val();
+  console.log('date is: ' + current_date);
+  console.log('symbol is: ' + current_symbol);
+  console.log('price is: ' + current_price);
+  console.log('Amount is: ' + current_units);
+
+
+
+  //Pushes the individual entry to the database. Push adds it as one 
+  //item with a single unique id.
+  database.ref().push({
+
+    date: current_date,
+    symbol: current_symbol,
+    price: current_price,
+    units: current_units,
+
+  });
+});
+
+//Displays the new employee upon being added. Takes only a snapshot of the 
+//added child.
+
+database.ref().on('child_added', function(child_snapshot){
+
+  //Gets the snapshot values.
+  current_date = child_snapshot.val().date;
+  current_symbol = child_snapshot.val().symbol;
+  current_price = child_snapshot.val().price;
+  current_units = child_snapshot.val().units;
+
+  //Creates the table row
+  var table_row = $('<tr>');
+
+  //Creates the name column.
+  var date_col = $('<td>').text(current_date);
+  date_col.attr('date', 'date');
+  table_row.append(date_col);
+
+  //Creates the role column.
+  var symbol_col = $('<td>').text(current_symbol);
+  symbol_col.attr('symbol', current_symbol);
+  symbol_col.append(symbol_col);
+
+  //Creates the start date column.
+  var price_col = $('<td>').text(current_price);
+  price_col.attr('price', current_price);
+  table_row.append(price_col);
+
+
+  //Creates the Monthy Rate column.
+  var units_col = $('<td>').text(current_units);
+  units_col.attr('units', current_units);
+  table_row.append(units_col);
+
+  //Creates the total billed column.
+  console.log((coins_price[current_symbol] - current_price) * current_units);
+  var net_gain_loss = (coins_price[current_symbol] - current_price) * current_units;
+  console.log(net_gain_loss);
+  var gain_loss_col = $('<td>').text(net_gain_loss);
+  gain_loss_col.attr('net-gain-loss', net_gain_loss);
+  table_row.append(gain_loss_col);
+
+  //Appends entire row to the table.
+  $('#emp-table').append(table_row);
+
+}, function(errorObject){
+
+    console.log("The read failed: " + errorObject.code);
+
+});
 
 window.setInterval(function(){
 
